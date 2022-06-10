69fe84ff59047f4c01a092783da999d00d120875
KNP <68104572+Nguyen070401@users.noreply.github.com>
2022-06-10 19:45:49 +0700

    Update JewelryPageProducts.js

--- src/components/JewelryPageProducts.js
+++ src/components/JewelryPageProducts.js
@@ -6,7 +6,7 @@ import styles from "./JewelryPageProducts.module.css"
 const JewelryPageProducts = () => {
   return (
     <>
-    <div className={styles.Title}>JEWELRY PAGE PRODUCTS</div>
+    <div className={styles.Title}>CÁC SẢN PHẨM TRANG SỨC</div>
     <div className={styles.Container}>
         {JewelryProducts.map((item)=>(
                 <Product item={item} key={item.id}/>
