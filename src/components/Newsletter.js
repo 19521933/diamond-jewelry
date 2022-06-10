57e162e5988af4dccc564c5b1d129bec2807e34d
KNP <68104572+Nguyen070401@users.noreply.github.com>
2022-06-10 20:51:14 +0700

    Update Newsletter.js

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -5,10 +5,10 @@ import styles from "./Newsletter.module.css"
 const Newsletter = () => {
   return (
     <div className={styles.Container}>
-      <h1 className={styles.Title}>Newsletter</h1>
-      <div className={styles.Description}>Get timely updates from your favorite products.</div>
+      <h1 className={styles.Title}>Hộp Thư Góp Ý</h1>
+      <div className={styles.Description}>Hãy gửi ý kiến của bạn vào hộp thư này.</div>
       <div className={styles.InputContainer}>
-        <input className={styles.Input} placeholder="Your email"></input>
+        <input className={styles.Input} placeholder="Email của bạn"></input>
         <button className={styles.Button}>
             <Send/>
         </button>
