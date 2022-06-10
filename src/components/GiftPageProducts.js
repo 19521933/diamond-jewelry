46c5f27e829a8a92e6233490f05eab1ee98ff2d2
KNP <68104572+Nguyen070401@users.noreply.github.com>
2022-06-10 19:44:30 +0700

    Update GiftPageProducts.js

--- src/components/GiftPageProducts.js
+++ src/components/GiftPageProducts.js
@@ -6,7 +6,7 @@ import styles from "./GiftPageProducts.module.css"
 const GiftPageProducts = () => {
   return (
     <>
-    <div className={styles.Title}>GIFT PAGE PRODUCTS</div>
+    <div className={styles.Title}>CÁC SẢN PHẨM QUÀ TẶNG</div>
     <div className={styles.Container}>
         {GiftProducts.map((item)=>(
                 <Product item={item} key={item.id}/>
