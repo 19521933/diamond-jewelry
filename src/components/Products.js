f0eeae467e6e93486d349cf0fa7623972e5d25a0
KNP <68104572+Nguyen070401@users.noreply.github.com>
2022-06-11 10:57:55 +0700

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,6 +6,7 @@ import styles from "./Products.module.css"
 const Products = () => {
   return (
     <>
+      <div className={styles.Title}>CÁC SẢN PHẨM NỔI BẬT</div>
       <div className={styles.Container}>
         {popularProducts.map((item) => (
           <Product item={item} key={item.id} />
