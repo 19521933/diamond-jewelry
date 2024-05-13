a7d7c7f3369e700e03d68b73142ad60d844c48f6
varia <ltnhan2610@gmail.com>
2024-05-13 15:24:12 +0700

    fix favorite page layout

--- src/pages/LikePage.js
+++ src/pages/LikePage.js
@@ -8,75 +8,77 @@ import Footer from '../components/Footer';
 import NoProductFound from '../components/NoProductFound';
 
 export default function LikePage() {
-	const [likeList, setLikeList] = useState([]);
+    const [likeList, setLikeList] = useState([]);
 
-	const userId = ls.get("userId");
-	const accessToken = ls.get("accessToken");
+    const userId = ls.get("userId");
+    const accessToken = ls.get("accessToken");
 
-	async function fetchData() {
-		const response = await axios.get(
-			process.env.REACT_APP_API_URL + `/users/likedProducts/${userId}`,
-			{ headers: { 'Authorization': 'Bearer ' + accessToken } });
-		setLikeList(response.data);
-	}
+    async function fetchData() {
+        const response = await axios.get(
+            process.env.REACT_APP_API_URL + `/users/likedProducts/${userId}`,
+            { headers: { 'Authorization': 'Bearer ' + accessToken } });
+        setLikeList(response.data);
+    }
 
-	useEffect(() => {
-		if (userId !== undefined) {
-			fetchData();
-		}
-	}, []);
+    useEffect(() => {
+        if (userId !== undefined) {
+            fetchData();
+        }
+    }, []);
 
-	// const onDeleteButtonClick = () => {
-	//   const response = axios({
-	//     method: 'put',
-	//     url: process.env.REACT_APP_API_URL + `/users/removeAllLikedProduct/${userId}`,
-	//     headers: { 'Authorization': 'Bearer ' + accessToken }
-	//   }).then(fetchData())
-	// }
+    // const onDeleteButtonClick = () => {
+    //   const response = axios({
+    //     method: 'put',
+    //     url: process.env.REACT_APP_API_URL + `/users/removeAllLikedProduct/${userId}`,
+    //     headers: { 'Authorization': 'Bearer ' + accessToken }
+    //   }).then(fetchData())
+    // }
 
-	// const onAddButtonClick = () => { }
+    // const onAddButtonClick = () => { }
 
-	const handleItem = useCallback(() => {
-		fetchData();
-	})
+    const handleItem = useCallback(() => {
+        fetchData();
+    })
 
-	return (
-		<>
-			<Header />
-			<div className={styles.like_container}>
-				<div className={styles.like_header}>
-					<ul>
-						<li>Trang chủ</li>
-						<li>Danh sách yêu thích</li>
-					</ul>
-					<h1>Danh sách yêu thích</h1>
-				</div>
+    return (
+        <div className={styles.container}>
+            <Header />
+            <div className={styles.like_container}>
+                <div className={styles.like_header}>
+                    <ul>
+                        <li>Trang chủ</li>
+                        <li>Danh sách yêu thích</li>
+                    </ul>
+                    <h1>Danh sách yêu thích</h1>
+                </div>
 
-				<div className={styles.like_body}>
-					<table className={styles.like_detail} cellPadding="5" cellSpacing="0" >
-						<thead>
-							<tr>
-								<th></th>
-								<th>Ảnh</th>
-								<th>Tên sản phẩm</th>
-								<th>Giá</th>
-								<th>Tình trạng</th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							{
-								likeList?.map(item =>
-									<LikeItem key={item.id} id={item.id} image={item.image} name={item.title} price={item.price}
-										avail={item.stock > 0} onItem={handleItem} />
-								)
-							}
-						</tbody>
-					</table>
-					{(likeList.length === 0) && <NoProductFound />}
-				</div>
-			</div>
-			<Footer />
-		</>
-	)
+                <div className={styles.like_body}>
+                    <table className={styles.like_detail} cellPadding="5" cellSpacing="0" >
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Ảnh</th>
+                                <th>Tên sản phẩm</th>
+                                <th>Giá</th>
+                                <th>Tình trạng</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                likeList?.map(item =>
+                                    <LikeItem key={item.id} id={item.id} image={item.image} name={item.title} price={item.price}
+                                        avail={item.stock > 0} onItem={handleItem} />
+                                )
+                            }
+                        </tbody>
+                    </table>
+                    {(likeList.length === 0) && <NoProductFound />}
+                </div>
+            </div>
+            <div className={styles.footer_container}>
+                <Footer />
+            </div>
+        </div>
+    )
 }
